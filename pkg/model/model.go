c4d6c6ef9d64ffe729c786da63f0ba50406184d0
Alexzzl <805119233@qq.com>
2025-03-13 11:37:54 +0800

    ssh配置

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -2,6 +2,7 @@
 package model
 
 import (
+	"goblog/pkg/database"
 	"goblog/pkg/logger"
 
 	"gorm.io/driver/mysql"
@@ -15,8 +16,11 @@ var DB *gorm.DB
 func ConnectDB() *gorm.DB {
 	var err error
 
+	// 使用已经通过 SSH 配置的 DSN
 	config := mysql.New(mysql.Config{
-		DSN: "root:zzl19961120...@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+		//DSN: "root:zzl19961120...@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+		// 使用自定义的 Dialector，它会通过 SSH 隧道连接
+		Conn: database.DB,
 	})
 
 	// 准备数据库连接池
